73aebef4669e8f6bc2a70f26d2088db634e211e2
JesperWerWer <zin4enko.bod@gmail.com>
2021-02-25 02:45:39 +0200

    LeaderBoard

--- The_Game/Models/Leaderboard.cs
+++ The_Game/Models/Leaderboard.cs
@@ -2,6 +2,10 @@
 {
     public class Leaderboard
     {
-        
+        public string Login { get; set; }
+        public int Wins { get; set; }
+        public int Loses { get; set; }
+        public int Draws { get; set; }
+        public int Total { get; set; }
     }
 }
--- The_Game/Services/LeaderboardStorage.cs
+++ The_Game/Services/LeaderboardStorage.cs
@@ -1,7 +1,25 @@
-﻿namespace The_Game.Services
+﻿using System.Collections.Concurrent;
+using System.Linq;
+using The_Game.Classes;
+using The_Game.Models;
+
+namespace The_Game.Services
 {
-    public class LeaderboardStorage
+    public class LeaderboardStorage : Storage<Leaderboard>
     {
-        
+        private readonly JsonWorker<Leaderboard> _jsonReader = new JsonWorker<Leaderboard>();
+        public LeaderboardStorage()
+        {
+
+            DataBase = _jsonReader.ReadList("Leaderboard.json").Result ?? new ConcurrentDictionary<int, Leaderboard>();
+        }
+
+        public bool FindUser(User user)
+        {
+            var find = DataBase.Select(x => x.Value).FirstOrDefault(x => x.Login == user.Login);
+            return find != null;
+        }
+
+
     }
 }
