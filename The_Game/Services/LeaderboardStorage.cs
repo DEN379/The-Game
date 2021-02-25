dea861e1f495be9cc7dfe48710036600a82125a7
JesperWerWer <zin4enko.bod@gmail.com>
2021-02-25 03:28:45 +0200

    Add LEaderBoard

--- The_Game/Services/LeaderboardStorage.cs
+++ The_Game/Services/LeaderboardStorage.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Concurrent;
 using System.Linq;
+using System.Threading.Tasks;
 using The_Game.Classes;
 using The_Game.Models;
 
@@ -18,6 +19,31 @@ namespace The_Game.Services
         {
             var find = DataBase.Select(x => x.Value).FirstOrDefault(x => x.Login == user.Login);
             return find != null;
+        }
+
+        public Task AddWins(string login)
+        {
+            var lead = DataBase.FirstOrDefault(x => x.Value.Login == login).Value;
+            lead.Wins++;
+            lead.Total++;
+            return Task.CompletedTask;
+            
+        }
+        public Task AddLoses(string login)
+        {
+            var lead = DataBase.FirstOrDefault(x => x.Value.Login == login).Value;
+            lead.Loses++;
+            lead.Total++;
+            return Task.CompletedTask;
+
+        }
+        public Task AddDraws(string login)
+        {
+            var lead = DataBase.FirstOrDefault(x => x.Value.Login == login).Value;
+            lead.Draws++;
+            lead.Total++;
+            return Task.CompletedTask;
+
         }
 
 
